6d8521f96158f1cc6223c48da2d0297d15e0003e
DongreShreyas <dshreyas13@gmail.com>
2025-02-04 09:48:32 +0530

    Requirements Updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Shreyas",
+    author_email="dshreyas13@gmail.com",
+    packages=find_packages()
+)
